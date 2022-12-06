d2a414dcf27b1537d08d4e39734a8ac734098bce
MarkosTh09 <markos.theocharis2009@gmail.com>
2022-12-06 12:47:56 +0400

    day 3

---
+++ src/bin/day3.rs
@@ -0,0 +1,61 @@
+use std::collections::{HashMap, HashSet};
+
+static LETTERS: &'static [char] = &[
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+];
+
+fn part1() -> usize {
+    let priorities: HashMap<char, usize> = {
+        let mut i = 0;
+        HashMap::from_iter(LETTERS.iter().map(|&x| {
+            i += 1;
+            (x, i)
+        }))
+    };
+
+    include_str!("day3-input.txt").lines().fold(0, |acc, x| {
+        let idx = x.len() / 2;
+        let p1 = &x[0..idx];
+        let p2 = &x[idx..];
+
+        let set: HashSet<char> = HashSet::from_iter(p1.chars());
+        let set2 = HashSet::from_iter(p2.chars());
+        let dup = set.intersection(&set2).next().unwrap();
+
+        acc + *priorities.get(&dup).unwrap()
+    })
+}
+
+fn part2() -> usize {
+    let lines: Vec<&str> = include_str!("day3-input.txt").lines().collect();
+
+    let priorities: HashMap<char, usize> = {
+        let mut i = 0;
+        HashMap::from_iter(LETTERS.iter().map(|&x| {
+            i += 1;
+            (x, i)
+        }))
+    };
+
+    lines.chunks(3).fold(0, |acc, elfs| {
+        let elf1: HashSet<char> = HashSet::from_iter(elfs[0].chars());
+        let elf2: HashSet<char> = HashSet::from_iter(elfs[1].chars());
+        let elf3: HashSet<char> = HashSet::from_iter(elfs[2].chars());
+
+        let i_set = elf1
+            .intersection(&elf2)
+            .map(|&c| c)
+            .collect::<HashSet<char>>();
+
+        acc + *priorities
+            .get(i_set.intersection(&elf3).next().unwrap())
+            .unwrap()
+    })
+}
+
+fn main() {
+    println!("part 1: {}", part1());
+    println!("part 2: {}", part2());
+}
